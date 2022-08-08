4ff838e6d2b59464ff814e0ddc786c94e64d7866
Ubuntu <ubuntu@ip-172-31-41-148.us-west-2.compute.internal>
2022-08-08 20:56:42 +0000

    Adding MVP. Want to add a few more features.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,83 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          name: 'Walk the Cat',
+          id: 15288170523890,
+          completed: false
+        },
+        {
+          name: 'Wash the Roof',
+          id: 1528817063421,
+          completed: false
+        }
+
+      ]
+    }
+  }
+
+  handleAdd = (name, completed) => {
+
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false
+    }
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+  }
+
+  handleToggle = (clickedId) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(todo => {
+        if(todo.id === clickedId){
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo;
+      })
+    })
+  }
+
   render() {
+    const { todos } = this.state;
     return (
       <div>
-        Todo App
+        <h1> TODO LIST </h1>
+        <TodoList handleToggle = {this.handleToggle} todos = {todos}/>
+        <Form handleAdd={this.handleAdd} />
+        <button onClick = {this.handleClear}>Clear</button>
       </div>
     )
   }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export default class Todo extends React.Component {
-  render() {
-    return (
-      <div>
-        Todo
-      </div>
-    )
+
+  handleClick = () => {
+    this.props.handleToggle(this.props.todo.id);
   }
+
+  render(){
+    return (<li onClick = {this.handleClick}>{this.props.todo.name} {this.props.todo.completed ? <span>- Completed!</span> : <span></span>}</li>);
+  };
 }
